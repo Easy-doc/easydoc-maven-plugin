7e948eeb502eca49083b187cd63e0bad3845cedd
stalary <452024236@qq.com>
2019-01-04 15:42:01 +0800

    MOD:增加报错信息

--- src/main/java/com/stalary/SaveFile.java
+++ src/main/java/com/stalary/SaveFile.java
@@ -17,8 +17,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * SaveFile
@@ -28,6 +28,8 @@ import java.util.regex.Pattern;
  */
 @Mojo(name = "save", defaultPhase = LifecyclePhase.PACKAGE)
 public class SaveFile extends AbstractMojo {
+
+    private static Logger log = Logger.getLogger("SaveFile");
 
     /** 扫描路径 **/
     @Parameter(property = "path")
@@ -64,53 +66,59 @@ public class SaveFile extends AbstractMojo {
     }
 
     private void file2String(List<File> fileList) {
-            StringBuilder sb = new StringBuilder();
-            pathMapper(fileList);
-            sb.append(JSONObject.toJSONString(PATH_MAP)).append(",,,");
-            for (File file : fileList) {
-                String fileName = file.getName();
-                String name =  fileName.substring(0, fileName.indexOf("."));
-                sb.append(name).append("~~");
-                // 读取单个文件
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("UTF-8")))) {
-                    String temp;
-                    StringBuilder curSb = new StringBuilder();
-                    while ((temp = reader.readLine()) != null) {
-                        curSb.append(temp);
-                    }
-                    sb.append(matching(curSb));
-                    // 每个文件以@@@分割
-                    sb.append("@@@");
-                } catch (Exception e) {
-                    e.printStackTrace();
+        StringBuilder sb = new StringBuilder();
+        pathMapper(fileList);
+        sb.append(JSONObject.toJSONString(PATH_MAP)).append(",,,");
+        for (File file : fileList) {
+            String fileName = file.getName();
+            String name = fileName.substring(0, fileName.indexOf("."));
+            sb.append(name).append("~~");
+            // 读取单个文件
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), Charset.forName("UTF-8")))) {
+                String temp;
+                StringBuilder curSb = new StringBuilder();
+                while ((temp = reader.readLine()) != null) {
+                    curSb.append(temp);
                 }
-            }
-            String fileName = System.getProperty("user.dir").replaceAll("\\.", "/") + "/src/main/resources/easydoc.txt";
-            // 直接使用FileWriter默认使用（ISO-8859-1 or US-ASCII）西方编码，中文会乱码
-            try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), Charset.forName("UTF-8")))) {
-                // 退出时存储消息
-                writer.write(sb.toString());
-                writer.flush();
+                sb.append(matching(curSb, name));
+                // 每个文件以@@@分割
+                sb.append("@@@");
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+        String fileName = System.getProperty("user.dir").replaceAll("\\.", "/") + "/src/main/resources/easydoc.txt";
+        // 直接使用FileWriter默认使用（ISO-8859-1 or US-ASCII）西方编码，中文会乱码
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), Charset.forName("UTF-8")))) {
+            // 退出时存储消息
+            writer.write(sb.toString());
+            writer.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private StringBuilder matching(StringBuilder str) {
+    private StringBuilder matching(StringBuilder str, String name) {
         String regex = "(?<!:)\\/\\/.*|\\/\\*(\\s|.)*?\\*\\/";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(str);
+        Matcher matcher = RegularExpressionUtils.createMatcherWithTimeout(str.toString(), regex, 200);
         StringBuilder sb = new StringBuilder();
         while (matcher.find()) {
-            String temp = matcher
-                    .group()
-                    .replaceAll("\\/\\/[^\n]*", "")
-                    .replaceAll("\\/\\*\\*", "")
-                    .replaceAll("\\*\\/", "")
-                    .replaceAll("\\*", "")
-                    .replaceAll(" +", " ");
-            // 每次匹配以~~分割
-            sb.append(temp).append("~~");
+            String temp = "";
+            try {
+                temp = matcher
+                        .group()
+                        .replaceAll("\\/\\/[^\n]*", "")
+                        .replaceAll("\\/\\*\\*", "")
+                        .replaceAll("\\*\\/", "")
+                        .replaceAll("\\*", "")
+                        .replaceAll(" +", " ");
+                // 每次匹配以~~分割
+                sb.append(temp).append("~~");
+            } catch (Exception e) {
+                // skip
+                log.warning("matcher error, please check it. skip..." + "\n" + name + " : " + temp);
+                log.warning("error: " + e);
+            }
         }
         return sb;
     }
